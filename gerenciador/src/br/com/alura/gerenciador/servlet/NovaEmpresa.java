f95d3fed80b6a0c1d4815d7f29b98ef90c690a5b
Charles Luxinger <charlesluxinger@hotmail.com>
2019-08-10 15:24:38 -0300

    5. Limitando o Servlet a receber apenas requisições Post

--- gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresa.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresa.java
@@ -17,7 +17,7 @@ public class NovaEmpresa extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response)
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
 		String nomeEmpresa = request.getParameter("nome");
