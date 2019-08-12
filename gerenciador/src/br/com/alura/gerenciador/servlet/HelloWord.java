a79b5f6ad72575381c6e51885fcf1b9a3fd54486
Charles Luxinger <charlesluxinger@hotmail.com>
2019-08-11 22:43:26 -0300

    17. Config através do WEB.xml

--- gerenciador/src/br/com/alura/gerenciador/servlet/HelloWord.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/HelloWord.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = "/hello")
 public class HelloWord extends HttpServlet{
 
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException{
 		
